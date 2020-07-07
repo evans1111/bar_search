a99cfd2f7a79814b36148e47b93bdd5c602afb40
Alex <aeboston1@gmail.com>
2020-07-07 01:25:22 -0400

    added user password reset email functionality

--- trip_planner/settings.py
+++ trip_planner/settings.py
@@ -135,3 +135,11 @@ LOGIN_REDIRECT_URL = 'blog-home'
 
 # Set the login route
 LOGIN_URL = 'login'
+
+# PW reset email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('db_user')
+EMAIL_HOST_PASSWORD = os.environ.get('db_pass')
